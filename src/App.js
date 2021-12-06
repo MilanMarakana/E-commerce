94a81c490f840ccba0d23a8e8fa31075c1764efa
MilanMarakana <milan.marakana7@gmail.com>
2021-12-06 21:54:20 +0100

    added react-router-dom v6 to the project

--- src/App.js
+++ src/App.js
@@ -1,11 +1,34 @@
 import React from 'react';
+import {Routes, Route} from 'react-router-dom';
+
 import './App.css';
 import HomePage from './pages/homepage/homepage';
 
+const Hats = (props) => {
+  console.log(props);
+  return (
+    <div>
+      <h1>Hats Page</h1>
+    </div>
+  );
+};
+
+const Jackets = (props) => {
+  return (
+    <div>
+      <h1>Jackets Page</h1>
+    </div>
+  );
+};
+
 function App() {
   return (
-    <div className='App'>
-      <HomePage />
+    <div>
+      <Routes>
+        <Route path='/' element={<HomePage />} />
+        <Route path='/hats' element={<Hats />} />
+        <Route path='/jackets' element={<Jackets />} />
+      </Routes>
     </div>
   );
 }
