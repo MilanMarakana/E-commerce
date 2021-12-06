45e20c1519e0dd879df1aefccfe472ae47f0b276
MilanMarakana <milan.marakana7@gmail.com>
2021-12-06 23:32:12 +0100

    added Shop and Collection component

--- src/App.js
+++ src/App.js
@@ -2,32 +2,33 @@ import React from 'react';
 import {Routes, Route} from 'react-router-dom';
 
 import './App.css';
+
 import HomePage from './pages/homepage/homepage';
+import Shop from './pages/shop/shop';
 
-const Hats = (props) => {
-  console.log(props);
-  return (
-    <div>
-      <h1>Hats Page</h1>
-    </div>
-  );
-};
+// const Hats = (props) => {
+//   console.log(props);
+//   return (
+//     <div>
+//       <h1>Hats Page</h1>
+//     </div>
+//   );
+// };
 
-const Jackets = (props) => {
-  return (
-    <div>
-      <h1>Jackets Page</h1>
-    </div>
-  );
-};
+// const Jackets = (props) => {
+//   return (
+//     <div>
+//       <h1>Jackets Page</h1>
+//     </div>
+//   );
+// };
 
 function App() {
   return (
     <div>
       <Routes>
         <Route path='/' element={<HomePage />} />
-        <Route path='/hats' element={<Hats />} />
-        <Route path='/jackets' element={<Jackets />} />
+        <Route path='/shop' element={<Shop />} />
       </Routes>
     </div>
   );
