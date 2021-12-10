bbd3eeb0909ac8d5d68a650d46f23ecf0a4ef0a1
MilanMarakana <milan.marakana7@gmail.com>
2021-12-10 21:42:56 +0100

    updated with sign in component and sign-in with google

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import {Routes, Route} from 'react-router-dom';
+import { Routes, Route } from 'react-router-dom';
 
 import './App.css';
 
@@ -7,6 +7,8 @@ import HomePage from './pages/homepage/homepage';
 import Shop from './pages/shop/shop';
 import SignInAndSignUp from './pages/signin-and-signup';
 import Header from './components/header/header';
+import { auth } from './firebase/firebase.utils';
+import SignIn from './components/sing-in/sign-in';
 
 // const Hats = (props) => {
 //   console.log(props);
@@ -25,17 +27,42 @@ import Header from './components/header/header';
 //   );
 // };
 
-function App() {
-  return (
-    <div>
-      <Header />
-      <Routes>
-        <Route path='/' element={<HomePage />} />
-        <Route path='/shop' element={<Shop />} />
-        <Route path='/contact' element={<SignInAndSignUp />} />
-      </Routes>
-    </div>
-  );
+class App extends React.Component {
+  constructor() {
+    super();
+
+    this.state = {
+      currentUser: null,
+    };
+  }
+
+  unsubscribeFromAuth = null;
+
+  componentDidMount() {
+    this.unsubscribeFromAuth = auth.onAuthStateChanged((user) => {
+      this.setState({ currentUser: user });
+
+      console.log(user);
+    });
+  }
+
+  componentWillUnmount() {
+    this.unsubscribeFromAuth();
+  }
+
+  render() {
+    return (
+      <div>
+        <Header currentUser={this.state.currentUser} />
+        <Routes>
+          <Route path='/' element={<HomePage />} />
+          <Route path='/shop' element={<Shop />} />
+          <Route path='/contact' element={<SignInAndSignUp />} />
+          <Route path='/sign-in' element={<SignIn />} />
+        </Routes>
+      </div>
+    );
+  }
 }
 
 export default App;
