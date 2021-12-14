78501b732220a6d1f17b0fd9f0ec75ea87c2cbac
MilanMarakana <milan.marakana7@gmail.com>
2021-12-14 01:02:08 +0100

    updated remove item functionality in checkout-item component

--- src/redux/cart/cart-reducer.js
+++ src/redux/cart/cart-reducer.js
@@ -19,6 +19,15 @@ const cartReducer = (state = INITIAL_STATE, action) => {
         ...state,
         cartItems: addToCartItem(state.cartItems, action.payload),
       };
+
+    case CartActionTypes.CLEAR_ITEM_FROM_CART:
+      return {
+        ...state,
+        cartItems: state.cartItems.filter((cartItem) => {
+          return cartItem.id !== action.payload.id;
+        }),
+      };
+
     default:
       return state;
   }
