ca524578ecfbd46a4f5a0209d07d85831fa160ef
MilanMarakana <milan.marakana7@gmail.com>
2021-12-30 23:14:16 +0100

    implemented a server for stripe API using node and express lib

--- client/src/components/stripe/stripe-checkout-button.jsx
+++ client/src/components/stripe/stripe-checkout-button.jsx
@@ -1,15 +1,36 @@
 import React from 'react';
 import StripeCheckout from 'react-stripe-checkout';
+import axios from 'axios';
 
-const onToken = (token) => {
-  console.log(token);
-  alert('Your Payment is successfully done.');
-};
+// const onToken = (token) => {
+//   console.log(token);
+//   alert('Your Payment is successfully done.');
+// };
 
 const StripeCheckoutButton = ({ price }) => {
   const priceForStripe = price * 100;
   const publishableKey =
     'pk_test_51K8QxMEaf38bD88AYND5LJWPH5RFOsUSuudUNyd0CB4e2HRy5MIGCws459oZ8VlqejOOjpn4f3l4hj6FxNOtwri1008M4i4GBd';
+
+  const onToken = (token) => {
+    axios({
+      url: 'payment',
+      method: 'post',
+      data: {
+        amount: priceForStripe,
+        token: token,
+      },
+    })
+      .then((response) => {
+        alert('Your payment has been successfully done.');
+      })
+      .catch((error) => {
+        console.log('Payment Error: ', error);
+        alert(
+          'There was an issue with your payment! Please make sure you use the provided credit card.'
+        );
+      });
+  };
 
   return (
     <StripeCheckout
