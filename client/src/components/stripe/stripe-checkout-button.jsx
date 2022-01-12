4ff149af97ef5a18d122811f2c507f05c083ba57
MilanMarakana <milan.marakana7@gmail.com>
2022-01-12 16:32:02 +0100

    implement a cart future that has use pervious log with signin and signout

--- client/src/components/stripe/stripe-checkout-button.jsx
+++ client/src/components/stripe/stripe-checkout-button.jsx
@@ -10,27 +10,27 @@ import axios from 'axios';
 const StripeCheckoutButton = ({ price }) => {
   const priceForStripe = price * 100;
   const publishableKey =
-    'pk_test_51K8QxMEaf38bD88AYND5LJWPH5RFOsUSuudUNyd0CB4e2HRy5MIGCws459oZ8VlqejOOjpn4f3l4hj6FxNOtwri1008M4i4GBd';
+		'pk_test_51K8QxMEaf38bD88AYND5LJWPH5RFOsUSuudUNyd0CB4e2HRy5MIGCws459oZ8VlqejOOjpn4f3l4hj6FxNOtwri1008M4i4GBd';
 
-  const onToken = (token) => {
-    axios({
-      url: 'payment',
-      method: 'post',
-      data: {
-        amount: priceForStripe,
-        token: token,
-      },
-    })
-      .then((response) => {
-        alert('Your payment has been successfully done.');
-      })
-      .catch((error) => {
-        console.log('Payment Error: ', error);
-        alert(
-          'There was an issue with your payment! Please make sure you use the provided credit card.'
-        );
-      });
-  };
+	const onToken = (token) => {
+		axios({
+			url: 'payment',
+			method: 'post',
+			data: {
+				amount: priceForStripe,
+				token: token,
+			},
+		})
+			.then((response) => {
+				alert('Your payment has been successfully done.');
+			})
+			.catch((error) => {
+				console.log('Payment Error: ', error);
+				alert(
+					'There was an issue with your payment! Please make sure you use the provided credit card.'
+				);
+			});
+	};
 
   return (
     <StripeCheckout
